43188d5972db9ee546c7e204b0e1dfe3b7f8cea2
jagger-harris <jagger.lee.harris@gmail.com>
2024-06-08 19:16:59 -0500

    Never minimize clients

--- .config/awesome/config/clients.lua
+++ .config/awesome/config/clients.lua
@@ -4,9 +4,9 @@ local awful = require("awful")
 require("awful.autofocus")
 
 client.connect_signal("manage", function(c)
-  if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-    awful.placement.no_offscreen(c)
-  end
+	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+		awful.placement.no_offscreen(c)
+	end
 end)
 
 -- DWM keep master stack
@@ -19,4 +19,9 @@ end)
 --- Mouse sloppy focus ----
 client.connect_signal("mouse::enter", function(c)
 	c:activate({ context = "mouse_enter", raise = false })
+end)
+
+--- Never minimize clients ---
+client.connect_signal("property::minimized", function(c)
+	c.minimized = false
 end)
