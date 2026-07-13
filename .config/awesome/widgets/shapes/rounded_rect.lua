daff5b0d766efb8dd0eb435a53f5e01f861f923b
jagger-harris <jagger.lee.harris@gmail.com>
2026-07-13 17:23:21 -0500

    Removed AwesomeWM

--- .config/awesome/widgets/shapes/rounded_rect.lua
+++
@@ -1,8 +0,0 @@
-local beautiful = require("beautiful")
-local gears = require("gears")
-
-local function rounded_rect(cr, width, height)
-  gears.shape.rounded_rect(cr, width, height, beautiful.rounded_corners)
-end
-
-return rounded_rect
